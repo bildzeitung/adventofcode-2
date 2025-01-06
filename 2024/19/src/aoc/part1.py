8da520083927dcceb6efef22228b7e3903d9cf1a
bildzeitung <bildzeitung@gmail.com>
2025-01-06 18:10:14 -0500

    day 19

--- 2024/19/src/aoc/part1.py
+++ 2024/19/src/aoc/part1.py
@@ -2,29 +2,59 @@
 Advent of Code Part 1
 """
 
+from functools import cache
 from typing import TextIO
+
 from rich.progress import track
 
-def solve(available : list[str], to_find: str) -> bool:
-    """ Return True if to_find can be made of available; False otherwise """
+max_lookup_size: int
+lookup_towels: dict[int, set[str]] = {}
+
+
+@cache
+def solve(to_find: str) -> bool:
+    """Return True if to_find can be made of available; False otherwise
+
+    Use a recursive depth-first search by longest prefix
+    """
+    global max_lookup_size
+    global lookup_towels
+
     # if nothing to match, then yay!
     if not to_find:
         return True
-    
-    # recursive search
-    return any(solve(available, to_find[len(i):]) for i in available if to_find.startswith(i))
+
+    for i in reversed(range(1, min(max_lookup_size, len(to_find)) + 1)):
+        if to_find[:i] in lookup_towels[i]:
+            rv = solve(to_find[i:])
+            if rv:
+                return rv
+
+    return False
 
 
 def main(f: TextIO) -> None:
     """
     Solution to part 1
     """
-    available_towels = next(f).strip().replace(' ', '').split(',')
-    
+    global max_lookup_size
+    global lookup_towels
+    available_towels = next(f).strip().replace(" ", "").split(",")
+
+    # let's order them by length
+    for towel in available_towels:
+        towel_length = len(towel)
+        if towel_length not in lookup_towels:
+            lookup_towels[towel_length] = set()
+        lookup_towels[towel_length].add(towel)
+    # print(lookup_towels)
+    max_lookup_size = max(lookup_towels)
+    # print(f"Largest match: {max_lookup_size}")
+
     next(f)  # blank
 
     all_lines = [line.strip() for line in f]
-    for line in all_lines:
-        print(f"{line} -> {solve(available_towels, line)}")
-    #total = sum(solve(available_towels, line) for line in track(all_lines))
-    #print(f"Total: {total}")
+    # for line in all_lines:
+    #    print(f"{line} -> {solve(line)}")
+    total = sum(solve(line) for line in track(all_lines))
+    print(f"Total: {total}")
--- 2024/19/src/aoc/part2.py
+++ 2024/19/src/aoc/part2.py
@@ -2,10 +2,58 @@
 Advent of Code Part 2
 """
 
+from functools import cache
 from typing import TextIO
+
+from rich.progress import track
+
+max_lookup_size: int
+lookup_towels: dict[int, set[str]] = {}
+
+
+@cache
+def solve(to_find: str) -> int:
+    """Return True if to_find can be made of available; False otherwise
+
+    Use a recursive depth-first search by longest prefix
+    """
+    global max_lookup_size
+    global lookup_towels
+
+    # if nothing to match, then yay!
+    if not to_find:
+        return 1
+
+    total = 0
+    for i in reversed(range(1, min(max_lookup_size, len(to_find)) + 1)):
+        if to_find[:i] in lookup_towels[i]:
+            total += solve(to_find[i:])
+
+    return total
 
 
 def main(f: TextIO) -> None:
     """
     Solution to part 2
     """
+    global max_lookup_size
+    global lookup_towels
+    available_towels = next(f).strip().replace(" ", "").split(",")
+
+    # let's order them by length
+    for towel in available_towels:
+        towel_length = len(towel)
+        if towel_length not in lookup_towels:
+            lookup_towels[towel_length] = set()
+        lookup_towels[towel_length].add(towel)
+    # print(lookup_towels)
+    max_lookup_size = max(lookup_towels)
+    # print(f"Largest match: {max_lookup_size}")
+
+    next(f)  # blank
+
+    all_lines = [line.strip() for line in f]
+    # for line in all_lines:
+    #    print(f"{line} -> {solve(line)}")
+    total = sum(solve(line) for line in track(all_lines))
+    print(f"Total: {total}")
