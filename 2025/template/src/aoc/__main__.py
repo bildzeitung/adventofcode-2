e7ac7f56c18079daf96ea4e2bc20ea4d7db6a541
bildzeitung <bildzeitung@gmail.com>
2025-12-04 19:43:44 -0500

    Use a specific data directory

--- 2025/template/src/aoc/__main__.py
+++ 2025/template/src/aoc/__main__.py
@@ -16,10 +16,10 @@ app = typer.Typer()
 @app.command()
 def part1(filename: Path):
     """Advent of Code: Part 1"""
-    p1.main(filename.open("r"))
+    p1.main((Path("data") / filename).open("r"))
 
 
 @app.command()
 def part2(filename: Path):
     """Advent of Code: Part 2"""
-    p2.main(filename.open("r"))
+    p2.main((Path("data") / filename).open("r"))
