e808b45b5d3eb972451a2df4dbf1c554ae7cdfcf
bildzeitung <bildzeitung@gmail.com>
2025-12-17 17:10:09 -0500

    day 5 part 2

--- 2025/05/src/aoc/part2.py
+++ 2025/05/src/aoc/part2.py
@@ -2,42 +2,37 @@
 Advent of Code Part 2
 """
 
-from typing import TextIO, Set
+
+from typing import TextIO
+
+type Pair = tuple[int, int]
+type PairList = list[Pair]
 
 
 def main(f: TextIO) -> None:
     """
     Solution to part 2
     """
-    all_ranges = []
+    all_ranges: PairList = []
     for line in f:
         if not line.strip():
             break
+
         start, stop = [int(x) for x in line.strip().split("-")]
         all_ranges.append((start, stop))
-    
-    '''
-    The naive approach:
 
-    all_numbers : set[int] = set()
-    for r in all_ranges:
-        all_numbers |= set(x for x in range(r[0], r[1]+1))
+    all_ranges = sorted(all_ranges)
+    print(all_ranges)
 
-    print(f"Total: {len(all_numbers)}")
-    '''
-    
-    ''' 
-        Cases:
-
-    1)  |----|
-          |----|
-
-    2)    |----|
-        |----|
-        
-    3)    |-|
-        |----|
-
-    4)  |----|
-          |-|
-    '''
+    finalRanges: PairList = []
+    current: Pair = all_ranges[0]
+    for item in all_ranges[1:]:
+        if current[0] <= item[0] <= current[1]:
+            current = (current[0], max(current[1], item[1]))
+        else:
+            finalRanges.append(current)
+            current = item
+    finalRanges.append(current)
+    print(finalRanges)
+    print(f"Original: {len(all_ranges)} Reduced: {len(finalRanges)}")
+    print(f"Total: {sum(i[1] - i[0] + 1 for i in finalRanges)}")
