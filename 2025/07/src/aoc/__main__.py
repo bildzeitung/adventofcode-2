976c93c6075d735009cf525791c94779822d9f24
bildzeitung <bildzeitung@gmail.com>
2025-12-17 22:49:00 -0500

    Lint

--- 2025/07/src/aoc/__main__.py
+++ 2025/07/src/aoc/__main__.py
@@ -2,7 +2,6 @@
 Advent of Code 2025
 """
 
-import sys
 from pathlib import Path
 
 import typer
--- 2025/07/src/aoc/part1.py
+++ 2025/07/src/aoc/part1.py
@@ -2,37 +2,38 @@
 Advent of Code Part 1
 """
 
-from typing import TextIO
 import re
-from collections import defaultdict
+from typing import TextIO
+
 from rich import print
+
 
 def main(f: TextIO) -> None:
     """
     Solution to part 1
     """
     beams: set[int] = set()
-    puzzle = []
+    puzzle: list[str] = []
     for line in f:
         line = line.strip()
         if "S" in line:
-            beams.add(line.index('S'))
+            beams.add(line.index("S"))
             continue
 
-        if '^' not in line:
+        if "^" not in line:
             continue
 
         puzzle.append(line)
 
     print(puzzle)
     print(f"Start: {beams}")
-    splits : int = 0
+    splits: int = 0
     for i in puzzle:
         for splitter in re.finditer(r"\^", i):
             pos = splitter.span()[0]
             if pos in beams:
-                beams.add(pos-1)
-                beams.add(pos+1)
+                beams.add(pos - 1)
+                beams.add(pos + 1)
                 beams.remove(pos)
-                splits += 1                
+                splits += 1
         print(f"Beams: {beams} | Splits: {splits}")
