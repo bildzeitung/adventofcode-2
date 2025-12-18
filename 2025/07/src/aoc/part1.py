12719a1a7af7fd0816a72204a20d2a7f5df28f68
bildzeitung <bildzeitung@gmail.com>
2025-12-17 22:46:42 -0500

    day 7 part 1

---
+++ 2025/07/src/aoc/part1.py
@@ -0,0 +1,38 @@
+"""
+Advent of Code Part 1
+"""
+
+from typing import TextIO
+import re
+from collections import defaultdict
+from rich import print
+
+def main(f: TextIO) -> None:
+    """
+    Solution to part 1
+    """
+    beams: set[int] = set()
+    puzzle = []
+    for line in f:
+        line = line.strip()
+        if "S" in line:
+            beams.add(line.index('S'))
+            continue
+
+        if '^' not in line:
+            continue
+
+        puzzle.append(line)
+
+    print(puzzle)
+    print(f"Start: {beams}")
+    splits : int = 0
+    for i in puzzle:
+        for splitter in re.finditer(r"\^", i):
+            pos = splitter.span()[0]
+            if pos in beams:
+                beams.add(pos-1)
+                beams.add(pos+1)
+                beams.remove(pos)
+                splits += 1                
+        print(f"Beams: {beams} | Splits: {splits}")
